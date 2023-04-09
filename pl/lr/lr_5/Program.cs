abb4f1c5e7c30f367690d79885e54903da47cee1
slev-v <stasrudometkin2@gmail.com>
2023-04-09 21:57:28 +0300

    add lr_5

---
+++ pl/lr/lr_5/Program.cs
@@ -0,0 +1,24 @@
+﻿class lr_5
+{
+    static void Main()
+    {
+        int[,] matrix = new int[10, 10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                matrix[i, j] = ((i + j) % 10) + 1;
+            }
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

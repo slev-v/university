285b2a6102fdbb0b9191ec719a1c2a373743c714
slev-v <120958402+slev-v@users.noreply.github.com>
2023-04-09 15:09:48 +0300

    create pr_7

---
+++ pl/pr_7/Program.cs
@@ -0,0 +1,63 @@
+class pr_3
+{
+    static void Main()
+    {
+        Console.Write("Введите номер задания: ");
+        int n = int.Parse(Console.ReadLine());
+        if (n == 1) first();
+        else second();
+        Console.ReadKey();
+    }
+
+
+    // Функия создает массив с случайными элементами и вызывает рекурсивную функцию
+    static void second()
+    {
+        int[] arr = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(-256, 257);
+            Console.Write(arr[i] + " ");
+        }
+        int sum = Recursion_second(arr);
+        Console.WriteLine("\n" + sum);
+    }
+
+
+    // Рекурсивная функция для нахождения суммы положительных элементов в массиве
+    static int Recursion_second(int[] arr, int index = 0)
+    {
+        if (index >= arr.Length) return 0;
+        else
+        {
+            int sum = Recursion_second(arr, index + 1);
+            if (arr[index] > 0) sum += arr[index];
+            return sum;
+        }
+    }
+
+
+    // Функция для приема значений и вызова рекурсивной функции
+    static void first()
+    {
+        Console.Write("Начало прогрессии: ");
+        double start = double.Parse(Console.ReadLine());
+        Console.Write("Знаменатель прогрессии: ");
+        double q = double.Parse(Console.ReadLine());
+        Console.Write("Количество элементов прогрессии: ");
+        int c = int.Parse(Console.ReadLine());
+        Console.WriteLine(Recursion_first(start, q, c));
+
+    }
+
+    // Рекурсивная функция для нахождения произведения геометрической прогрессии
+    static double Recursion_first(double start, double q, int c)
+    {
+        if (c == 0) return 1;
+        else
+        {
+            return Recursion_first(start, q, c - 1) * (start * Math.Pow(q, c - 1));
+        }
+    }
+}

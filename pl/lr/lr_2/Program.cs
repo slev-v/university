d3ef092b308291896c8d2e0eb20a49e0e1fbe31b
slev-v <stasrudometkin2@gmail.com>
2023-04-09 21:04:36 +0300

    add lr_2

--- pl/lr/lr_2/Program.cs
+++ pl/lr/lr_2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿class lr_2
+{
+    static void Main()
+    {
+        Console.Write("Введите a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Введите b: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("Введите c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        if ((Math.Pow(b, 2) - 4 * a * c) < 0)
+        {
+            Console.WriteLine("Уравнение не имеет действительных корней");
+        }
+        Console.ReadKey();
+    }
+}

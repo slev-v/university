58d36f4100c86bf85b15d99af91c90872cb679a8
slev-v <stasrudometkin2@gmail.com>
2023-04-09 22:30:16 +0300

    add lr_7

---
+++ pl/lr/lr_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+class lr_7
+{
+    static void Main()
+    {
+        Console.Write("Введите текст: ");
+        string st = Console.ReadLine();
+        string[] words = st.Split(' ');
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (string word in words)
+        {
+            if (word != "hello")
+            {
+                result.Append(word);
+                result.Append(' ');
+            }
+        }
+
+        Console.WriteLine(result.ToString());
+        Console.ReadKey();
+    }
+}

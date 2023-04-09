4295eaeefcf30615ef4811cea1eb1cff8a367201
slev-v <stasrudometkin2@gmail.com>
2023-04-09 20:53:53 +0300

    reorganized the folder structure & add lr_1

---
+++ pl/lr/lr_1/Program.cs
@@ -0,0 +1,11 @@
+﻿class lr_1
+{
+    static void Main()
+    {
+        Console.Write("Введите x: ");
+        double x = double.Parse(Console.ReadLine());
+        double res = 2 * Math.Pow(x, 4) - 3 * Math.Pow(x, 3) - 5 * x + 6;
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
